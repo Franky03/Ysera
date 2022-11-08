f412dc1492b816759068e2812f2cb05371559085
Frankley Kaiky <kaikym20@gmail.com>
2022-11-08 19:04:24 -0300

    trying fix my bug

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import sys
 import multiprocessing as mp
 
 if __name__ == '__main__':
-    try:
-        set_start_method('spawn')
-    except RuntimeError:
-        pass
+    # try:
+    #     set_start_method('spawn')
+    # except RuntimeError:
+    #     pass
     start_time = time.time()
     print(np.__version__)
 
@@ -34,20 +34,16 @@ if __name__ == '__main__':
     mythread(New, params, 7031, 10545, "arquivo3", string3)
     mythread(New, params, 10547, 14060, "arquivo4", string4)
 
-    """
-    Pool method not working
+    # pool = Pool(processes=4)
 
-    pool = Pool(processes=4)
-
-    r1 = pool.apply_async(mythread, (New, params, 0, 3515, "arquivo1", string))
-    r2 = pool.apply_async(mythread, (New, params, 3516, 7031, "arquivo2", string2))
-    r3 = pool.apply_async(mythread, (New, params, 7031, 10545, "arquivo3", string3))
-    r4 = pool.apply_async(mythread, (New, params, 10547, 14060, "arquivo4", string4))
+    # r1 = pool.apply_async(mythread, args= (New, params, 0, 3515, "arquivo1", string))
+    # r2 = pool.apply_async(mythread, args= (New, params, 3516, 7031, "arquivo2", string2))
+    # r3 = pool.apply_async(mythread, args= (New, params, 7031, 10545, "arquivo3", string3))
+    # r4 = pool.apply_async(mythread, args= (New, params, 10547, 14060, "arquivo4", string4))
 
     # pool.close()
     # pool.join()
 
-    """
     print("---%s seconds ---" % (time.time() - start_time))
 
 #Linhas do arquivo 2 = 177, 614, 1289
