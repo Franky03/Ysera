e56f587d53c834d591bd1e9cd8d4a6824546bdde
Frankley Kaiky <kaikym20@gmail.com>
2023-01-10 12:45:39 -0300

    organizando

--- teste.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-with open("./output/arquivo1.txt") as arq:
-    f= pd.DataFrame(data= arq ,columns=["TIPO_INTERAÇÃO, TIPO_ATOMO1, RESIDUO_AMINOACIDO1, CADEIA_AMINOACIDO1, TIPO_ATOMO2, RESIDUO_AMINOACIDO2, CADEIA_AMINOACIDO2, DISTANCIA"])
-
-print(f.head())
-
-
